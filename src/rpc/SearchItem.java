aacf548ae0858198773f08a0c7de7dc525f1b540
ZhaoruiDing <zhaorui1225@gmail.com>
2018-03-15 14:39:41 -0500

    frontend favorite shows correctly when shows the search items

--- src/rpc/SearchItem.java
+++ src/rpc/SearchItem.java
@@ -3,6 +3,7 @@ package rpc;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -38,6 +39,8 @@ public class SearchItem extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userId = request.getParameter("user_id");
+
 		double lat = Double.parseDouble(request.getParameter("lat"));
 		double lon = Double.parseDouble(request.getParameter("lon"));
 		// term can be empty or null.
@@ -47,12 +50,18 @@ public class SearchItem extends HttpServlet {
 		
 		DBConnection connection = DBConnectionFactory.getDBConnection();
 		List<Item> items = connection.searchItems(lat, lon, term);
+		
+		
+		Set<String> favorite = connection.getFavoriteItemIds(userId);
 
 		JSONArray array = new JSONArray();
 		try {
 			for (Item item : items) {
 				// Add a thin version of item object
 				JSONObject obj = item.toJSONObject();
+				// Check if this is a favorite one.
+				// This field is required by frontend to correctly display favorite items.
+				obj.put("favorite", favorite.contains(item.getItemId()));
 				array.put(obj);
 			}
 		} catch (Exception e) {
